3f119b0431d2e0772b80d1065f9ccf84cd6fc339
laedall <dallenbachlaetitia@gmail.com>
2026-06-02 14:16:28 +0200

    ajout_last_projet
    dernières modifications

---
+++ MiniTriangleDVD.js
@@ -0,0 +1,40 @@
+let posX = 200;
+let posY = 200;
+let posR = 200;
+
+let size = 70;
+
+let vitX = 3.123;
+let vitY = 5.4321;
+let vitR = 1;
+
+function setup() {
+  frameRate(60);
+  createCanvas(windowWidth,windowHeight);
+  background(rgba(0,0,0,1));
+}
+
+function draw() {
+  stroke(posR,12,234);
+  fill(posR,123,234);
+  
+   posX+= vitX;
+  posY += vitY
+  
+  posR += vitR;
+  if( posR >= 255 || posR <=0){
+    vitR = vitR * -1;
+  }
+ 
+  
+  if( posX >= width-size|| posX <=0){
+    vitX= vitX * -1;
+  }
+  
+    if( posY >= height-size|| posY <=0){
+    vitY= vitY * -1;
+  }
+  
+  triangle(posX, posY, size);
+
+}
